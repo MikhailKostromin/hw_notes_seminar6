88c31c1d8bf0489d28156c7912ba0392dda6f84e
mihailkostromin <kostrominmi@mail.ru>
2024-02-11 13:33:21 +0300

    Add note controller

---
+++ src/main/java/com/example/hw_notes_seminar6/controller/NoteController.java
@@ -0,0 +1,51 @@
+package com.example.hw_notes_seminar6.controller;
+
+import com.example.hw_notes_seminar6.model.Note;
+import com.example.hw_notes_seminar6.service.NoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notes")
+@RequiredArgsConstructor
+public class NoteController {
+
+    private final NoteService noteService;
+
+    @GetMapping
+    public ResponseEntity<List<Note>> getAll() {
+        return new ResponseEntity<>(noteService.getAllNotes(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Note> createNote(@RequestBody Note note) {
+        return new ResponseEntity<>(noteService.createNote(note), HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Note> getNote(@PathVariable("id") Long id) {
+        Note noteById;
+        try {
+            noteById = noteService.getNoteById(id);
+        } catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Note());
+        }
+        return new ResponseEntity<>(noteById, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Note> updateNote(@RequestBody Note note) {
+        return new ResponseEntity<>(noteService.updateNote(note), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteNote(@PathVariable("id") Long id){
+        noteService.deleteNote(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
